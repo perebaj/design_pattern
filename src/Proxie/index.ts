b3dd1b3cd12010785e68a5b477212dc2e0734cf7
Jonathan Silva <perebaj@gmail.com>
2020-12-29 13:59:18 -0300

    refatoring files and add proxie patter

---
+++ src/Proxie/index.ts
@@ -0,0 +1,46 @@
+interface Subject {
+    request():void
+}
+
+class RealSubject implements Subject{
+    request(): void {
+        console.log('ReadSubject: Handling request');
+    }
+
+}
+
+class Proxie implements Subject {
+    private realSubject: RealSubject;
+
+    constructor(realSubject: RealSubject) {
+        this.realSubject = realSubject;
+    }
+    public request(): void {
+        if (this.checkAccess()) {
+            this.realSubject.request();
+            this.logAccess();
+        }
+    }
+
+    private checkAccess(): boolean {
+        console.log('Proxy: Checking access prior to firing a real request.');
+
+        return true;
+    }
+
+    private logAccess(): void {
+        console.log('Proxy: Logging the time of request.');
+    }
+}
+
+function clientCode(subject: Subject){
+    subject.request();
+}
+
+console.log('Client:Executing the client code with a real subject');
+const realSubject = new RealSubject();
+clientCode(realSubject)
+console.log('')
+console.log('Client: Executing the same client code with a proxy:');
+const proxy = new Proxie(realSubject)
+clientCode(proxy)
